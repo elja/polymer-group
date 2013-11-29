56c988cffc650fec4c315df53fb145169eebfbc6
elja <a.eliseev@itransition.com>
2013-11-29 20:18:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolymerGroup::Application.config.secret_token = '4c08f661898d1face808d032dd7db7993d30956b04b3c2cc3c1dd31cd770245669e168299680fcec87e652056284ee12683ba02a11089d7808b9e4aa95a74ef4'
