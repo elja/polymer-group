c756e99a71467de07f3133bb0999fe6a3f961399
elja <a.eliseev@itransition.com>
2014-02-06 00:29:49 +0300

    v8 engine added

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module PolymerGroup
     config.assets.enabled = true
 
     # Do not connect to the database while assets compiling
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
