c4474f4f45837f81062fdd3d45926d78b48606cb
elja <a.eliseev@itransition.com>
2014-02-06 00:52:00 +0300

    production assets fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ PolymerGroup::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,15 @@ PolymerGroup::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += [
+      "refinery/*",
+      "refinery/icons/*",
+      "wymeditor/lang/*",
+      "wymeditor/skins/refinery/*",
+      "wymeditor/skins/refinery/**/*",
+      "modernizr-min.js",
+      "admin.js"
+  ]
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
